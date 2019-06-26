9be8980d3c530697fd1df562653938c6dfdb9b74
Jebikoh <aneeshedara@gmail.com>
2019-06-25 23:19:58 -0400

    Fix Incorrect Favicon & Icons

--- config/website.js
+++ config/website.js
@@ -3,14 +3,14 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Hack3 - A Modern Hackathon', // Navigation and Site Title
+  siteTitle: 'Hack3 - Experience Innovation', // Navigation and Site Title
   siteTitleAlt: 'Hack3', // Alternative Site title for SEO
   siteTitleShort: 'Hack3', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteHeadline: 'Experience innovation.', // Headline for schema.org JSONLD
   siteUrl: 'https://hack3.co', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  siteDescription: 'A new hackathon in the Boston Area',
   author: 'Hack3 Team', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
